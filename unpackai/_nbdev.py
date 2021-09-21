568a55aaaffebef0e81f4a1db6d921c5cd5a4ebf
raynardj <b2ray2c@gmail.com>
2021-09-21 13:05:52 +0800

    🤫 hush!

--- unpackai/_nbdev.py
+++ unpackai/_nbdev.py
@@ -4,6 +4,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"check_img": "10_utils.ipynb",
          "clean_error_img": "10_utils.ipynb",
+         "hush": "10_utils.ipynb",
          "CosineSearch": "15_interp_latent.ipynb",
          "InterpEmbeddings": "15_interp_latent.ipynb",
          "ImageLabeler": "20_Image_Labeler.ipynb",
