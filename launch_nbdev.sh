b0bc2576591d9f5bff512793d246c2d9e5428625
Jean-Francois Thuong <jfthuong@gmail.com>
2021-09-23 23:22:27 +0800

    #21 Truncated pictures can be deleted * Corrected bug * Added tests for clean directory * Corrected possibility to run tests from notebook with ipytest

--- launch_nbdev.sh
+++ launch_nbdev.sh
@@ -2,3 +2,6 @@ nbdev_build_lib
 nbdev_clean_nbs
 # We want to have a README managed independently
 nbdev_build_docs --mk_readme False
+# Extract Test Cases
+cd $(dirname "$0")
+python3 test/extract_tests.py
