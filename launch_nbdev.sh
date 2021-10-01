c7fee1a94be3b32245ec3f67b7fda3b0cf9fc117
raynardj <b2ray2c@gmail.com>
2021-10-01 23:49:07 +0800

    👻 prompt more info when build

--- launch_nbdev.sh
+++ launch_nbdev.sh
@@ -1,7 +1,19 @@
+echo '========== 🏗 build unpackai *.py files =========='
 nbdev_build_lib
+echo '========== ✅  =========='
+echo '========== 🧼 clean notebooks =========='
 nbdev_clean_nbs
 # We want to have a README managed independently
+echo '========== ✅  =========='
+echo '========== 📚 build unpackai documentations =========='
 nbdev_build_docs --mk_readme False
+echo '========== ✅  =========='
+echo "========== 🧪 extract tests under $(dirname $0) =========="
 # Extract Test Cases
 cd $(dirname "$0")
 python3 test/extract_tests.py
+echo '========== ✅  =========='
+# Reinstall the package
+echo '========== 📦 reinstall unpackai =========='
+pip install -e .
+echo '========== ✅  =========='
